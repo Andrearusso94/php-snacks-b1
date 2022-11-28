692ff505ff93dbc12ab4685fdebd91bffee7071e
Andrearusso94 <andrea.r.4845@gmail.com>
2022-11-28 12:29:07 +0100

    add my solution

--- snak1/index.php
+++ snak1/index.php
@@ -1,18 +1,35 @@
 <?php
-?>
+//Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario. Ogni array avrà una squadra di casa e una squadra ospite, 
+//punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. Stampiamo a schermo tutte le partite con questo schema.
 
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
-
-</body>
-
-</html>
+//Olimpia Milano - Cantù | 55-60
+$matches = [
+    [
+        $sCasa = 'Olimpia Milano',
+        $sTrasferta = 'Cantù',
+        $puntiCasa = 80,
+        $puntiTrasferta = 50,
+    ],
+    [
+        $sCasa = 'Olimpia Milano',
+        $sTrasferta = 'Barca',
+        $puntiCasa = 20,
+        $puntiTrasferta = 50,
+    ],
+    [
+        $sCasa = 'Olimpia Milano',
+        $sTrasferta = 'Liverpool',
+        $puntiCasa = 60,
+        $puntiTrasferta = 40,
+    ],
+];
+var_dump($matches);
+//creo ciclo foreach per stampare le partite
+foreach ($matches as $match) {
+    foreach ($match as $key => $value) {
+        echo $value . ' ';
+        if ($key == count($match) - 1) {
+            echo '<br>';
+        }
+    }
+}
